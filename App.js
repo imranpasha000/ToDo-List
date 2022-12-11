2c88efb1cd9abff194aee921f62f65b4e0f71383
MOHAMMAD IMRAN <pashaturki19081@gmail.com>
2022-12-11 20:46:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+//import Todo from "./components/todoreact/todo";
+import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
